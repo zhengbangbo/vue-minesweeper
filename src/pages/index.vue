9c4f5d59dd62e181aa00ae5438de0f27a0ef23fd
赴春夏 <zhengbangbo@hotmail.com>
2022-03-11 03:38:04 +0800

    chore: readme and eslint

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,7 +15,8 @@ const state = reactive(
   Array.from({ length: HEIGHT }, (_, y) =>
     Array.from({ length: WIDTH },
       (_, x): BlockState => ({
-        x, y,
+        x,
+        y,
         adjacentMines: 0,
         revealed: false,
       }),
@@ -25,9 +26,8 @@ const state = reactive(
 
 function generateMines() {
   for (const row of state) {
-    for (const block of row) {
+    for (const block of row)
       block.mine = Math.random() < 0.2
-    }
   }
 }
 
@@ -56,18 +56,18 @@ const numberColors = [
 function updateNumbers() {
   state.forEach((row, y) => {
     row.forEach((block, x) => {
-      if (block.mine) 
+      if (block.mine)
         return
-      
+
       directions.forEach(([dx, dy]) => {
         const x2 = x + dx
         const y2 = y + dy
-        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT) 
+        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
           return
-        if (state[y2][x2].mine) 
+        if (state[y2][x2].mine)
           block.adjacentMines += 1
       })
-      })
+    })
   })
 }
 
@@ -91,30 +91,31 @@ updateNumbers()
   <div>
     Minesweeper
     <div p5>
-    <div v-for="row, y in state"
-      flex="~"
-      items-center justify-center
-      :key="y">
-
-      <button 
-      v-for="block, x in row"
-      :key="x"
-      flex="~"
-      items-center justify-center
-      w-10 h-10 m='0.5'
-      hover="bg-gray/25"
-      border="1.5 gray-400/10" 
-      :class="getBlockClass(block)"
-      @click="onClick(block)"
+      <div
+        v-for="row, y in state"
+        :key="y"
+        flex="~" items-center
+        justify-center
       >
-      <template v-if="block.revealed">
-        <div v-if="block.mine" i-mdi:mine/>
-        <div v-else>
-          {{ block.adjacentMines }}
-        </div>
-      </template> 
-      </button>
+        <button
+          v-for="block, x in row"
+          :key="x"
+          flex="~"
+          items-center justify-center
+          w-10 h-10 m="0.5"
+          hover="bg-gray/25"
+          border="1.5 gray-400/10"
+          :class="getBlockClass(block)"
+          @click="onClick(block)"
+        >
+          <template v-if="block.revealed">
+            <div v-if="block.mine" i-mdi:mine />
+            <div v-else>
+              {{ block.adjacentMines }}
+            </div>
+          </template>
+        </button>
+      </div>
     </div>
-  </div>
   </div>
 </template>
